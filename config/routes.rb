63a86b5cfeff9e89d1b89bb46bba05bf51a90236
Colum O' Callaghan <camcallaghan@gmail.com>
2016-01-06 23:48:30 +0000

    Adding submit button to my comments

--- config/routes.rb
+++ config/routes.rb
@@ -29,7 +29,9 @@ Nomster::Application.routes.draw do
 
   root 'places#index'
 
-  resources :places
+  resources :places do
+    resources :comments, :only => :create
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # More comments here ......
 
