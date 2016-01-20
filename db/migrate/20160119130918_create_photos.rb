4dc4eef704e28fe683201e3fce213ea51ad8018a
Colum O' Callaghan <camcallaghan@gmail.com>
2016-01-20 21:01:57 +0000

    Updated migration

--- db/migrate/20160119130918_create_photos.rb
+++ db/migrate/20160119130918_create_photos.rb
@@ -2,9 +2,9 @@ class CreatePhotos < ActiveRecord::Migration
   def change
     create_table :photos do |t|
 
-    	add_column :photos, :picture, :string
-    	add_column :photos, :caption, :string
-    	add_column :photos, :place_id, :integer
+    	t.string :picture
+        t.string :caption
+    	t.integer :place_id
 
 
       t.timestamps
