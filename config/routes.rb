50245c74b21aff56649cf37423145034936ece8f
Colum O' Callaghan <camcallaghan@gmail.com>
2016-01-20 17:23:41 +0000

    Controller changes

--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Nomster::Application.routes.draw do
 
 
   resources :places do
-    resources :places,
+    resources :places, :only => :create
 
   end
   # The priority is based upon order of creation: first created -> highest priority.
