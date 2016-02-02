d6d5ea496bf712d3308fa3fdbb7e7b5d3691431c
Colum O' Callaghan <camcallaghan@gmail.com>
2016-02-02 22:03:37 +0000

    Controller tweaking

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,10 +1,10 @@
 class PhotosController < ApplicationController
 	before_action :authenticate_user!, :only => [:new, :create, :edit, :update, :destroy]
 	# a bunch of other code
-	
+
 	def create
     	@place = Place.find(params[:place_id])
-    	@place.photos.create(photo_params.merge(user: current_user))
+    	@place.photos.create(photo_params)
     	redirect_to place_path(@place)
   end
 
