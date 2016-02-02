89ef4c4cd8047d19d7e55227bf95f86de1165913
Colum O' Callaghan <camcallaghan@gmail.com>
2016-02-02 15:37:17 +0000

    Merging database values

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,12 @@
 class Photo < ActiveRecord::Base
 	belongs_to :user
 	has_one :place_id
+	has_many :photos
+
+	geocoded_by :address
+	after_validation :geocode
+	
+	validates :name, :presence => true, :length => { :minimum => 3}
+	validates :address, :presence => true
+	validates :description, :presence => true
 end
